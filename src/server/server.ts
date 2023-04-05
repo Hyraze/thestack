c421524259e07fabdb1053b191893d7282277a64
Hanish Rao <hanishrao321@gmail.com>
2023-04-05 17:48:30 +0530

    feat: :art: added view engine

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+
 const server = express()
 const port = 8080
 
@@ -7,9 +8,10 @@ server.use(express.static("dist"))
 server.set("view engine","ejs");
 
 server.use("/", (req,res)=> {
-    res.render("Hello World");
+    res.render("index",{
+        content: "EJS <em>TEST</em>"
+    });
 })
-
 
 server.listen(port,"0.0.0.0",()=>{
     console.info(`Port listing on port ${port}`)
