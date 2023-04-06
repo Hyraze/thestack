60eaefe7e1556e032d94a64175c11eacdf8c4cce
Hanish Rao <hanishrao321@gmail.com>
2023-04-06 07:38:49 +0530

    feat: :art: react setup

--- src/server/server.ts
+++ src/server/server.ts
@@ -14,7 +14,7 @@ server.set("view engine","ejs");
 
 server.use("/", (req,res)=> {
     res.render("index",{
-        content: "EJS <em>TEST</em>"
+        initialContent: "Loading.."
     });
 })
 
