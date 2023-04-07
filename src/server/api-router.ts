601f1f14f5cc6d083c90979b26e55c7463778c44
Hanish Rao <hanishrao321@gmail.com>
2023-04-07 08:01:28 +0530

    feat: :art: fetching data from db

--- src/server/api-router.ts
+++ src/server/api-router.ts
@@ -1,12 +1,33 @@
 import express from "express";
 import cors from "cors";
+import { connectClient } from "./db";
 const router = express.Router();
 router.use(cors());
 
-import testData from "../test/books.json";
+router.get("/books", async (req, res) => {
+  const client = await connectClient();
 
-router.get("/books", (req, res) => {
-  res.send({ books: testData });
+  const books = await client
+    .collection("books")
+    .find()
+    .project({
+      id: 1,
+      description: 1,
+      author: 1,
+      title: 1,
+      _id:0
+    })
+    .toArray();
+
+  res.send({ books });
+});
+
+router.get("/books/:bookId", async (req, res) => {
+  const client = await connectClient();
+  const book = await client.collection("books").findOne({
+    id: req.params.bookId,
+  });
+  res.send({ book });
 });
 
 export default router;
